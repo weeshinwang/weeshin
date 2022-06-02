9e8e6ebeae6cd3da247a77b6541399a34c645820
weeshin <weeshin@foxmail.com>
2022-06-02 16:24:38 +0800

    fix: styling for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,14 +4,17 @@ import { graphql, Link } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import {} from "gatsby"
+import styled from "styled-components"
 const shortcodes = { Link } // Provide common components here
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
     <Layout title="about">
-      <MDXProvider components={shortcodes}>
-        <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
-      </MDXProvider>
+      <AboutPageWrapper>
+        <MDXProvider components={shortcodes}>
+          <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </AboutPageWrapper>
     </Layout>
   )
 }
@@ -27,4 +30,11 @@ export const pageQuery = graphql`
       }
     }
   }
+`
+const AboutPageWrapper = styled.div`
+  margin: 0 auto;
+  margin-top: 32px;
+  min-width: 300px;
+  max-width: 700px;
+  padding: 0 16px;
 `
